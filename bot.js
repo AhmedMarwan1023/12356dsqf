4b832bb1c593b204588384358f626e7eba752366
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2019-03-25 13:48:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,19 +4,19 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("559378584284954654")
+let channel =     client.channels.get("549319064162664468")
 setInterval(function() {
 channel.send(` Shanks Shanks Shanks Shanks`);
 }, 30)
 })
 
-    // +say
-      if (command === "say") {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-      if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-              message.delete()
-        message.channel.sendMessage(args.join(" "))
-      }
-       
-      
- client.login(process.env.BOT_TOKEN);
+client.on('message',function(message) {
+	let prefix = "!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`**# ${args}**`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+client.login(process.env.BOT_TOKEN);
